78649f68f403f710f5b3aaf8dd75d18ecc0b9f50
Reyhan Gedik <334480@student.mboutrecht.nl>
2022-11-02 13:16:25 +0100

    map gedaan
    ik heb de mappen gedaan

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+    //Database constanten
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'mvc-oop-toets');  //
+
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    define('URLROOT', 'http://mvc-oop-toets.com/'); //
+
+    define('SITENAME', 'mvc')
+?>
